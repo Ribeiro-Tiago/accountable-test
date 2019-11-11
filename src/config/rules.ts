4eb8d9f40a485f9611a67364a9579b94fe601849
Tiago Ribeiro <tribeiro@wemanity.com>
2019-11-11 20:39:18 +0100

    rules updates - added possible items to configs/rules - exported items individually

--- src/config/rules.ts
+++ src/config/rules.ts
@@ -1,32 +1,41 @@
-export default {
-	currencies: {
-		coal: "kg",
-		cheese: "kg",
-		book: "quantity",
-		bike: "quantity"
-	},
-	max_per_person: {
-		coal: .1,
-		bike: 2,
-		buy_bike_in_row: 2,
-		sell_book_in_row: 3,
-	},
-	buy: {
-		coal: .75,
-		cheese: .15,
-		bike: .15,
-		book: 0
-	},
-	sell: {
-		coal: .15,
-		cheese: .05,
-		bike: .05,
-		book: .05
-	},
-	ownage: {
-		coal_per_bike: -.05,
-		coal_per_bike_limit: 0,
-		book_per_coal: .05,
-		book_per_coal_limit: .5,
-	}
+export const items = [
+	"book",
+	"bike",
+	"cheese",
+	"coal"
+];
+
+export const currencies = {
+	coal: "kg",
+	cheese: "kg",
+	book: "quantity",
+	bike: "quantity"
+};
+
+export const maxPerPerson = {
+	coal: .1,
+	bike: 2,
+	buy_bike_in_row: 2,
+	sell_book_in_row: 3,
+};
+
+export const buy = {
+	coal: .75,
+	cheese: .15,
+	bike: .15,
+	book: 0
+};
+
+export const sell = {
+	coal: .15,
+	cheese: .05,
+	bike: .05,
+	book: .05
+};
+
+export const ownage = {
+	coal_per_bike: -.05,
+	coal_per_bike_limit: 0,
+	book_per_coal: .05,
+	book_per_coal_limit: .5,
 };
