d695de42071b4e565ca729637289cc60f4c1afa6
Tiago Ribeiro <tribeiro@wemanity.com>
2019-11-11 21:46:05 +0100

    global market - added schema for global market

---
+++ src/models/globalMarket.model.ts
@@ -0,0 +1,21 @@
+import { Schema, model } from "mongoose";
+import { ItemType } from "./offer.model";
+
+export const globalMarketSchema = new Schema({
+	item: {
+		type: String,
+		required: true
+	},
+	quantity: {
+		type: Number,
+		required: true
+	}
+});
+
+export interface IGlobalMarket {
+	_id: string;
+	quantity: number;
+	item: ItemType;
+}
+
+export default model("GlobalMarket", globalMarketSchema);
