a09cab167726d181d3ef181f888351a8d98ca26c
Tiago Ribeiro <tribeiro@wemanity.com>
2019-11-11 22:11:32 +0100

    added function to update govt balance

--- src/controllers/government.controller.ts
+++ src/controllers/government.controller.ts
@@ -1,16 +1,26 @@
 import Trader, { ITrader } from "../models/trader.model";
 import { formatCurrency } from "../utils/formatter/formatter";
 
+const GOVT_ID = "government";
+
 export const createGovt = async (): Promise<number> => {
-	return (await new Trader({ id: "government" }).save() as ITrader).cg;
+	return (await new Trader({ id: GOVT_ID }).save() as ITrader).cg;
 };
 
 export const getGovtBalance = async (): Promise<string> => {
-	const govt = await Trader.findOne({ id: "government" }, { _id: 0, cg: 1 }) as ITrader;
+	const govt = await Trader.findOne({ id: GOVT_ID }, { _id: 0, cg: 1 }) as ITrader;
 
 	if (!govt) {
 		return formatCurrency(await createGovt());
 	}
 
 	return formatCurrency(govt.cg);
+};
+
+export const updateGovtBalance = async (balance: number): Promise<void> => {
+	await Trader.findOneAndUpdate({ id: GOVT_ID }, {
+		$inc: {
+			cg: balance,
+		}
+	});
 };
