37da0b48c3c1848364b25d983c0265f9c16c7d62
Tiago Ribeiro <tribeiro@wemanity.com>
2019-11-11 19:59:37 +0100

    utils/exexuter - added a sort of middleware utils function to handle errs

--- src/routes/government.route.ts
+++ src/routes/government.route.ts
@@ -1,9 +1,10 @@
 import { Router, Response, Request } from "express";
 
 import * as controller from "../controllers/government.controller";
+import { executer } from "../utils/executer/executer";
 
 const router = Router();
 
-router.get("/balance", async (_req: Request, res: Response) => res.json(await controller.getGovtBalance()));
+router.get("/balance", async (_req: Request, res: Response) => executer(res, controller.getGovtBalance));
 
 export default router;
---
+++ src/utils/executer/executer.ts
@@ -0,0 +1,16 @@
+import { Response } from "express";
+
+// tslint:disable-next-line: ban-types
+export const executer = async (res: Response, func: Function, params?: string) => {
+	try {
+		res.json(await func(params));
+	} catch (err) {
+		if (err === 404) {
+			res.status(404);
+			res.json("Resource not found");
+		} else {
+			res.status(500);
+			res.json("Could not proccess request");
+		}
+	}
+};
